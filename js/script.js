dfa2005823171793e2d63fef4e5865759b67036e
k1LoW <k1lowxb@gmail.com>
2012-08-31 22:57:08 +0900

    スポンサー情報を1つのエレメントhtmlからjsでうまく表示を変換するように実装

--- js/script.js
+++ js/script.js
@@ -2,6 +2,24 @@ $(function(){
     $('.carousel').carousel({
         interval: 8000
     });
+
+    // sponsor.html変換
+    if (location.href.match(/sponsor.html/)) {
+        // タイトル削除
+        $('#sponsor-title').hide();
+
+        $('.sponsor.span3').removeClass('span3').addClass('span12');
+
+        $('.sponsor.span2').removeClass('span2').addClass('span4');
+
+        // キャプション表示
+        $('.sponsor.span12 .thumbnail').addClass('row').removeClass('thumbnail').css({marginBottom: '40px'});
+        $('.sponsor.span12 .sponsor-logo').addClass('span3');
+        $('.sponsor.span12 .sponsor-caption').addClass('span8').removeClass('sponsor-caption');
+
+        $('.sponsor.span4 .thumbnail').removeClass('thumbnail').css({marginBottom: '40px'});
+        $('.sponsor.span4 .sponsor-caption').removeClass('sponsor-caption');
+    }
 });
 
 
