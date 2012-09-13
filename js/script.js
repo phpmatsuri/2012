535e04c04c11e661617a46c2c3e7c35a183ed064
k1LoW <k1lowxb@gmail.com>
2012-09-13 19:42:43 +0900

    個人スポンサー追加

--- js/script.js
+++ js/script.js
@@ -10,17 +10,28 @@ $(function(){
 
         $('.sponsor-title').show();
 
+        // platinum
         $('.sponsor.span3').removeClass('span3').addClass('span12');
 
+        // silver
         $('.sponsor.span2').removeClass('span2').addClass('span4');
 
+        $('.sponsor.span1').removeClass('span1').addClass('span2');
+
+
         // キャプション表示
+        // platinum
         $('.sponsor.span12 .thumbnail').addClass('row').removeClass('thumbnail').css({marginBottom: '40px'});
         $('.sponsor.span12 .sponsor-logo').addClass('span3');
         $('.sponsor.span12 .sponsor-caption').addClass('span8').removeClass('sponsor-caption');
 
+        // silver
         $('.sponsor.span4 .thumbnail').removeClass('thumbnail').css({marginBottom: '40px'});
         $('.sponsor.span4 .sponsor-caption').removeClass('sponsor-caption');
+
+        // personal
+        $('.sponsor-caption.personal').css({display:'inline'});
+        $('.sponsor.span2 .sponsor-caption').removeClass('sponsor-caption');
     } else if (location.href.match(/.html/) && !location.href.match(/index.html/)) {
         // top,sponser以外
         $('.sponsor').css({marginLeft: '0px'});
