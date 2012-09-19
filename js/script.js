6cfd8142d03bf696fcf9c51abc999d1dae90187a
k1LoW <k1lowxb@gmail.com>
2012-09-19 18:28:46 +0900

    スポンサーのデザイン調整

--- js/script.js
+++ js/script.js
@@ -5,33 +5,37 @@ $(function(){
 
     // sponsor.html変換
     if (location.href.match(/sponsor.html/)) {
+        // sponser
         // タイトル削除
         $('#sponsor-title').hide();
 
         $('.sponsor-title').show();
 
-        // platinum
+        // platinum, gold
+        $('#platinum').addClass('row-fluid');
+        $('#gold').addClass('row-fluid');
         $('.sponsor.span3').removeClass('span3').addClass('span12');
 
         // silver
-        $('.sponsor.span2').removeClass('span2').addClass('span4');
+        $('.silver-row').addClass('row-fluid');
+        $('.sponsor.silver').removeClass('span2').addClass('span4');
 
         // personal
+        $('#personal').addClass('row-fluid');
         $('.sponsor.span1').removeClass('span1').addClass('span2');
 
 
         // キャプション表示
-        // platinum
+        // platinum, gold
         $('.sponsor.span12 .thumbnail').addClass('row').removeClass('thumbnail').css({marginBottom: '40px'});
         $('.sponsor.span12 .sponsor-logo').addClass('span3');
         $('.sponsor.span12 .sponsor-caption').addClass('span8').removeClass('sponsor-caption');
 
         // silver
-        $('.sponsor.span4 .thumbnail').removeClass('thumbnail').css({marginBottom: '40px'});
-        $('.sponsor.span4 .sponsor-caption').removeClass('sponsor-caption');
+        $('.sponsor.silver .thumbnail').css({marginBottom: '40px'}).removeClass('thumbnail');
+        $('.sponsor.silver .sponsor-caption').removeClass('sponsor-caption');
 
         // personal
-        $('.sponsor-caption.personal').css({display:'inline'});
         $('.sponsor.span2 .sponsor-caption').removeClass('sponsor-caption');
     } else if (location.href.match(/.html/) && !location.href.match(/index.html/)) {
         // top, sponser以外
